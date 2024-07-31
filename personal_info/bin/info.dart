6b9d5946b60751eb8d2a5947043634ea8b8cf198
amer266030 <amer266030@users.noreply.github.com>
2024-07-31 17:44:56 +0300

    showInfo function

--- personal_info/bin/info.dart
+++ personal_info/bin/info.dart
@@ -16,4 +16,18 @@ class Info {
       ----------------------------
       ''');
   }
+
+  static void showInfo(String str) {
+    switch (str) {
+      case '1':
+        print('My name is Amer, \nI am 99yrs of age,\nI am a Computer Wizard!');
+      case '2':
+        print('I like the piano, weight lifting, billiards, and tennis');
+      case '3':
+        print('Most things related to Technology');
+      case '4':
+        print(
+            'I play music and video games in my free time. \nI like Snowboarding during the winter season, and Beach Resorts during summer.');
+    }
+  }
 }
