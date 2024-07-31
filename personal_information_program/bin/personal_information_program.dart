189a57d2f5ed277eeee0a55313dc0897aaae3404
basel_1422@outlook.com <basel_1422@outlook.com>
2024-07-31 22:14:40 +0300

    create dart project

---
+++ personal_information_program/bin/personal_information_program.dart
@@ -0,0 +1,5 @@
+import 'package:personal_information_program/personal_information_program.dart' as personal_information_program;
+
+void main(List<String> arguments) {
+  print('Hello world: ${personal_information_program.calculate()}!');
+}
---
+++ personal_information_program/test/personal_information_program_test.dart
@@ -0,0 +1,8 @@
+import 'package:personal_information_program/personal_information_program.dart';
+import 'package:test/test.dart';
+
+void main() {
+  test('calculate', () {
+    expect(calculate(), 42);
+  });
+}
