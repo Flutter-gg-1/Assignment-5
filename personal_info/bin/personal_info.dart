0ff4c915f5c6caeee61294fb618fc42abb544ee3
amer266030 <amer266030@users.noreply.github.com>
2024-07-31 17:49:07 +0300

    main function logic

--- personal_info/bin/personal_info.dart
+++ personal_info/bin/personal_info.dart
@@ -1,5 +1,19 @@
 import 'dart:io';
 
+import 'info.dart';
+
 void main() {
-  var userInput = stdin.readLineSync();
+  var shouldExit = false;
+  Info.showInstructions();
+
+  do {
+    var userInput = stdin.readLineSync();
+    if (userInput! == 'i') {
+      Info.showInstructions();
+    } else if (userInput == 'q') {
+      shouldExit = true;
+    } else {
+      Info.showInfo(userInput);
+    }
+  } while (!shouldExit);
 }
