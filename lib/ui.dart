1608329f9679a90f66dbb2e9cb15d3ff0641181b
Mahdi Al-Helal <mmahdi630@gmail.com>
2024-07-31 20:02:58 +0300

    Hobbies function has been add

--- lib/ui.dart
+++ lib/ui.dart
@@ -15,5 +15,39 @@ void titleCard() {
       print("*${" " * pading}$myName${" " * (pading + 1)}*");
     }
   }
-  stdout.write("*" * 22);
+  stdout.write("${"*" * 22}\n");
 }
+
+void hobbies() {
+  print("\n___");
+  print("[ |");
+  print("---");
+  print("My hobies");
+
+  print("WALKING");
+  print("  o ");
+  print(" /");
+  print(" /|");
+  print("${"*" * 14}\n");
+
+  print("Video games");
+
+  tv();
+
+  controller();
+}
+
+void controller() {
+  print("   ____");
+  print(r"  /0__0\");
+}
+
+void tv() {
+  print(" ______");
+  print("||    ||");
+  print("||    ||");
+  print(" ______");
+  print(r"   /\");
+}
+
+void selection() {}
