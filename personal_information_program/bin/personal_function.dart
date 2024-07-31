4dbeedff6a9aa93c25c6974c66cd17b7df31d4a3
Ali_Altarouty <tarooti14dev@gmail.com>
2024-07-31 17:06:21 +0300

    Display & Create Personal Function

---
+++ personal_information_program/bin/personal_function.dart
@@ -0,0 +1,5 @@
+personalFunction(
+    {required String name, required int age, required String occupation}) {
+  print("My name is $name,I'm $age years old.");
+  print("My occupation is $occupation");
+}
--- personal_information_program/bin/personal_information_program.dart
+++ personal_information_program/bin/personal_information_program.dart
@@ -1,3 +1,9 @@
+import './personal_function.dart';
+
 void main() {
-  print("Hello");
+  String name = "Ali Altarouty";
+  int age = 30;
+  String occupation = "Programming :)";
+
+  personalFunction(name: name, age: age, occupation: occupation);
 }
