1f58b461d92bcf7274095bbec57ea1b74a20b446
Abdulaziz_Alamri <aazizalamri5@gmail.com>
2024-07-31 18:26:49 +0300

    created personal_info_prog dart project

---
+++ personal_info_prog/bin/personal_info_prog.dart
@@ -0,0 +1,5 @@
+import 'package:personal_info_prog/personal_info_prog.dart' as personal_info_prog;
+
+void main(List<String> arguments) {
+  print('Hello world: ${personal_info_prog.calculate()}!');
+}
---
+++ personal_info_prog/test/personal_info_prog_test.dart
@@ -0,0 +1,8 @@
+import 'package:personal_info_prog/personal_info_prog.dart';
+import 'package:test/test.dart';
+
+void main() {
+  test('calculate', () {
+    expect(calculate(), 42);
+  });
+}
