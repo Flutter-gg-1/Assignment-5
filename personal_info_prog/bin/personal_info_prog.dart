f3b1d847c4356b7a0a049e098f7bb88d02db68f6
Abdulaziz_Alamri <aazizalamri5@gmail.com>
2024-07-31 19:10:09 +0300

    added menu and deleted lib folder

--- personal_info_prog/bin/personal_info_prog.dart
+++ personal_info_prog/bin/personal_info_prog.dart
@@ -1,5 +1,36 @@
-import 'package:personal_info_prog/personal_info_prog.dart' as personal_info_prog;
+import 'dart:io';
 
 void main(List<String> arguments) {
-  print('Hello world: ${personal_info_prog.calculate()}!');
+  bool isExit = false;
+
+  do {
+    switch (menu()) {
+      case '1': 
+      case '2':
+      case '3':
+      case '4':
+      case '0':
+        isExit = true;
+      default:
+    }
+  } while (!isExit);
+}
+
+menu() {
+  print('\n---- About me-----\n');
+  print('1: personal information');
+  print('2: Hobbies');
+  print('3: Interests');
+  print('4: If you would like to know more...');
+  print('0: exit');
+
+  stdout.write('What would you like to know? ');
+  String? choice = stdin.readLineSync();
+
+  if (choice != null && choice.isNotEmpty) {
+    return choice;
+  } else {
+    print('Invalid Input');
+    return '0';
+  }
 }
