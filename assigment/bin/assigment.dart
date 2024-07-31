c5a7466ddfcbfdb16f3b4e9e40921cfc19826160
muhannad <mohnsalzaher@gmail.com>
2024-07-31 16:37:00 +0300

    function Ginral information added

--- assigment/bin/assigment.dart
+++ assigment/bin/assigment.dart
@@ -1,5 +1,11 @@
 import 'package:assigment/assigment.dart' as assigment;
 
-void main(List<String> arguments) {
-  print('Hello world: ${assigment.calculate()}!');
+void main(List<String> arguments) {}
+
+//function that print a ginral information of the user
+gineralInformation(
+    {required String name, required int age, required String occupation}) {
+  print("your name is: $name ");
+  print("your age is: $age");
+  print("your occupation is: $occupation ");
 }
