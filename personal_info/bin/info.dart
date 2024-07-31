982a37438aaadfea6090f558388e7c8d4a3dd733
amer266030 <amer266030@users.noreply.github.com>
2024-07-31 17:33:50 +0300

    instructions function

---
+++ personal_info/bin/info.dart
@@ -0,0 +1,19 @@
+class Info {
+  static void showInstructions() {
+    print('''
+      ----------------------------
+      What would you like to know about me?
+      Enter a number from the list below:
+
+      1 | General Info
+      2 | Hobbies
+      3 | Interests
+      4 | Additional Info
+
+      i | Instructions
+      q | Quit
+
+      ----------------------------
+      ''');
+  }
+}
