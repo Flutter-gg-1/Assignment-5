902a31ec05a9cd16977b3fb58a79c93935088bbe
muhannad <mohnsalzaher@gmail.com>
2024-07-31 18:01:18 +0300

    Assiment completed

--- assigment/bin/assigment.dart
+++ assigment/bin/assigment.dart
@@ -1,68 +1,11 @@
+import 'ginral_info.dart';
+import 'hobbies.dart';
+import 'intrest_And_pation.dart';
+import 'addintonal_info.dart';
+
 void main(List<String> arguments) {
   gineralInformation(name: "muhannad", age: 27, occupation: "programmer");
   hobbies();
   interests();
-}
-
-//function that print a ginral information of the user
-gineralInformation(
-    //optional added to the functions
-    {required String? name,
-    required int? age,
-    required String? occupation}) {
-  print("your name is: $name ");
-  print("your age is: $age");
-  print("your occupation is: $occupation ");
-}
-
-//hobbies functions
-hobbies() {
-  //Map inside list to insert all hobbies with 2 kyes 1 of them for the hobby and the other for the description
-  //then I used for In to print the elments inside the map
-  List<Map<String, String>> hobbies = [
-    {
-      "Hobby": "Programming",
-      "description": "I like to progrraming in dart",
-    },
-    {
-      "Hobby": "gym",
-      "description": "liffting whgites",
-    },
-    {
-      "Hobby": "gaming",
-      "description": "I like play shoter games",
-    },
-  ];
-  for (var element in hobbies) {
-    print("${element["Hobby"]} : Description: ${element["description"]}");
-  }
-}
-
-//Map inside list to insert all hobbies with 2 kyes 1 of them for the hobby and the other for the description
-//then I used for In to print the elments inside the map
-interests() {
-  List<Map<String, String>> interests = [
-    {
-      "interests": "Mobail dev",
-      "description": "I am intersted in mobail app dev",
-    },
-    {
-      "interests": "Travel",
-      "description": "I like to visit countris",
-    },
-    {
-      "passions": "passions",
-      "description": "passions is dead",
-    },
-  ]; //for in to print interst
-  for (var element in interests) {
-    if (element.containsKey("interests")) {
-      print("${element["interests"]} : Description ${element["description"]}");
-    }
-  } //for in to print passtion
-  for (var element in interests) {
-    if (element.containsKey("passions")) {
-      print("${element["passions"]} : Description ${element["description"]}");
-    }
-  }
+  additionalInfo();
 }
---
+++ assigment/bin/ginral_info.dart
@@ -0,0 +1,10 @@
+//function that print a ginral information of the user
+gineralInformation(
+    //optional added to the functions
+    {required String? name,
+    required int? age,
+    required String? occupation}) {
+  print("your name is: $name ");
+  print("your age is: $age");
+  print("your occupation is: $occupation ");
+}
