a5708ddd72e13390da1b60343abe6efa041ac728
turki-aloufi <turkialoufi44@gmail.com>
2024-07-31 18:22:08 +0300

    hobbies

--- assignment/bin/assignment.dart
+++ assignment/bin/assignment.dart
@@ -1,13 +1,26 @@
 void main(List<String> arguments) {
   generalInfo();
+  hobies();
 }
 
 generalInfo() {
   String name = "turki";
   int age = 24;
   String occupation = "flutter developer";
-  print("---------General info---------");
+  print("\n\n---------General info---------");
   print("My name is: $name");
   print("My age is: $age");
   print("My occupation is: $occupation");
+  print("------------------------------");
+}
+
+hobies() {
+  Map<String, String> hobbies = {
+    "Reading": "I enjoy reading fiction and non-fiction books.",
+    "Traveling": "Exploring new places and cultures is a passion of mine.",
+    "Photography": "Capturing moments through my lens."
+  };
+  print('\n\n---------Hobbies---------');
+  hobbies.forEach((k, v) => print("$k: $v"));
+  print('-------------------------');
 }
