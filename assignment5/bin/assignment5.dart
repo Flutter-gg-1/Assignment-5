3e0c981a9effaf1ae2cd7ae0f5dfd7422ea905cc
Najwa-aldahri <aldahri.najwa@gmail.com>
2024-07-31 19:54:06 +0300

    adding hobbies function

--- assignment5/bin/assignment5.dart
+++ assignment5/bin/assignment5.dart
@@ -1,6 +1,14 @@
 import 'dart:io';
 
 import 'generalInfo.dart';
+import 'hobbies.dart';
+
+/*
+  General Information: Your name, age, and occupation.
+Hobbies: A list and description of your hobbies.
+Interests: Your interests and passions.
+Additional Information: Any other interesting details about yourself.
+*/
 
 void main(List<String> arguments) {
   print("---------------------------------------------------------");
@@ -10,22 +18,27 @@ void main(List<String> arguments) {
   bool condition = false;
   while (condition == false) {
     print("Do you want to know more about NAJWA?");
-    print("1-YES                 2-NO");
+    print("1-YES                          2-NO");
     int? answer = int.parse(stdin.readLineSync()!);
     if (answer == 1) {
       condition = true;
       print("Yaaay");
       print("what do you what to know about me?");
       print("1-general information");
+      print("2-Hobbies");
+      print("3-interests and passions");
       int? input = int.parse(stdin.readLineSync()!);
 
       switch (input) {
         case 1:
           generalInfo();
+
+        case 2:
+          hobbies();
       }
     } else if (answer == 2) {
-      print("Whhay -_-!!");
-      print("try again");
+      print("Whhay -_- !!");
+      print("try again\n");
     } else {
       print("somthing  whent wrong");
     }
---
+++ assignment5/bin/hobbies.dart
@@ -0,0 +1,5 @@
+hobbies() {
+  print("here are some of my hobbies");
+  List<String> hobbies = ["coloring", "coking","playing sport"];
+  print(hobbies);
+}
