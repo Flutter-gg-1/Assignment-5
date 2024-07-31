9d807e83263a3b0e6626952c8c77d427c7156c5c
amer266030 <amer266030@users.noreply.github.com>
2024-07-31 18:17:16 +0300

    code refactor2

--- personal_info/bin/colorful_print.dart
+++ personal_info/bin/colorful_print.dart
@@ -1,15 +1,7 @@
 import 'info.dart';
 
 extension ColorfulPrint on Info {
-  static void yellow(String text) {
-    print('\x1B[33m$text\x1B[0m');
-  }
-
-  static void green(String text) {
-    print('\x1B[32m$text\x1B[0m');
-  }
-
-  static void red(String text) {
-    print('\x1B[31m$text\x1B[0m');
-  }
+  static void yellow(String text) => print('\x1B[33m$text\x1B[0m');
+  static void green(String text) => print('\x1B[32m$text\x1B[0m');
+  static void red(String text) => print('\x1B[31m$text\x1B[0m');
 }
