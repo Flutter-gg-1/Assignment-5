e84b493c0b401f6289480c14744520ea1328eda2
Najwa-aldahri <aldahri.najwa@gmail.com>
2024-07-31 19:29:41 +0300

    forcing the user to answer feauter

--- assignment5/bin/assignment5.dart
+++ assignment5/bin/assignment5.dart
@@ -7,12 +7,27 @@ void main(List<String> arguments) {
   print("---------------> WELCOM TO NAJWA'S WORLD <---------------");
   print("---------------------------------------------------------");
 
-  print("what do you what to know about me?");
-  print("1-general info");
-  int? input = int.parse(stdin.readLineSync()!);
+  bool condition = false;
+  while (condition == false) {
+    print("Do you want to know more about NAJWA?");
+    print("1-YES                 2-NO");
+    int? answer = int.parse(stdin.readLineSync()!);
+    if (answer == 1) {
+      condition = true;
+      print("Yaaay");
+      print("what do you what to know about me?");
+      print("1-general information");
+      int? input = int.parse(stdin.readLineSync()!);
 
-  switch (input) {
-    case 1:
-      generalInfo();
+      switch (input) {
+        case 1:
+          generalInfo();
+      }
+    } else if (answer == 2) {
+      print("Whhay -_-!!");
+      print("try again");
+    } else {
+      print("somthing  whent wrong");
+    }
   }
 }
