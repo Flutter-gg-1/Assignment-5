c4b76cc6592d03998580d952172a131aa79acb3f
Amjad Elahi <amjadelahi07@gmail.com>
2024-07-31 19:21:20 +0300

    add general function

---
+++ assignment5/bin/assignment5.dart
@@ -0,0 +1,27 @@
+import 'dart:io';
+import 'general_information.dart';
+
+void main(List<String> arguments) {
+  bool isExit = false;
+  do {
+    print("\n\n");
+    print("______ ^_^ Hello, nice to meet you ^_^ ______");
+    print("1: General Information");
+    print("2: Hobbies");
+    print("3: Interests");
+    print("4: Additional Information");
+    print("5: Exit");
+    print("_____________________________________________");
+
+    int userInpu = int.parse(stdin.readLineSync()!);
+
+    switch (userInpu) {
+      case 1:
+        generalInformation();
+      case 2:
+      case 3:
+      case 4:
+      case 5:
+    }
+  } while (!isExit);
+}
---
+++ assignment5/bin/general_information.dart
@@ -0,0 +1,10 @@
+import 'dart:io';
+
+generalInformation(){
+  String name = "Amjad";
+  int age = 24;
+  String occupation = "student at tuwaiq Academy";
+
+  print("My name is $name. I'm $age years old and a $occupation.");
+  stdin.readLineSync();
+}
