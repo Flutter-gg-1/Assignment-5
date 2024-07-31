5c03517f8e6f40e330310892279d3be8b2ab50a2
turki-aloufi <turkialoufi44@gmail.com>
2024-07-31 18:07:03 +0300

    general info

---
+++ assignment/bin/assignment.dart
@@ -0,0 +1,13 @@
+void main(List<String> arguments) {
+  generalInfo();
+}
+
+generalInfo() {
+  String name = "turki";
+  int age = 24;
+  String occupation = "flutter developer";
+  print("---------General info---------");
+  print("My name is: $name");
+  print("My age is: $age");
+  print("My occupation is: $occupation");
+}
