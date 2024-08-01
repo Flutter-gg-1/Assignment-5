a4b120766bcec298d8082894c70d12eb5e64ab04
rawan <rawan1993_7@hotmail.com>
2024-08-01 09:25:53 +0300

    Editing

--- personal_info/bin/display_addtion_Info.dart
+++ personal_info/bin/display_addtion_Info.dart
@@ -1,10 +1,9 @@
-
 import 'colorprint.dart';
 
 display_addtioninfo() {
   printError("--------------------------");
-   printSuccess("I am a very passionate and creative person.");
-   printSuccess(
+  printSuccess("I am a very passionate and creative person.");
+  printSuccess(
       "My mind is full of great ideas and I'm always eager to learn new things and explore different areas");
-   printSuccess(""); // Empty line for spacing
+  printSuccess(""); // Empty line for spacing .
 }
