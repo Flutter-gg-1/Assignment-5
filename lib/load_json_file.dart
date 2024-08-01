e8b59ae99e149916580d7ab4f3ee91114e7d35eb
FaHaDoF69 <93477293+FaHaDoF69@users.noreply.github.com>
2024-08-01 08:10:15 +0300

    build fun to read json file

---
+++ lib/load_json_file.dart
@@ -0,0 +1,17 @@
+import 'dart:convert';
+import 'dart:io';
+
+/*
+*
+* Function takes json path and return it as a map
+*
+* */
+Future<Map<String, dynamic>> loadProfile({required String path}) async {
+  final file = File(path);
+  // wait reading file
+  final jsonStr = await file.readAsString();
+  // decode json
+  final jsonMap = jsonDecode(jsonStr);
+  // return my info as a map
+  return jsonMap;
+}
